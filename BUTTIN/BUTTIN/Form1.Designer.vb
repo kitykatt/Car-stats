84c735bee9f2bfdbbf26a94448511f39c702e46b
kitykatt <90800597+kitykatt@users.noreply.github.com>
2022-01-26 13:56:00 -0700

    Add files via upload

--- BUTTIN/BUTTIN/Form1.Designer.vb
+++ BUTTIN/BUTTIN/Form1.Designer.vb
@@ -30,11 +30,25 @@ Partial Class Form1
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(130, 117)
+        Me.Button1.Location = New System.Drawing.Point(130, 132)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 1
@@ -83,7 +97,7 @@ Partial Class Form1
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(130, 156)
+        Me.Button2.Location = New System.Drawing.Point(130, 161)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(75, 23)
         Me.Button2.TabIndex = 7
@@ -92,18 +106,109 @@ Partial Class Form1
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(130, 195)
+        Me.Button3.Location = New System.Drawing.Point(130, 190)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(75, 23)
         Me.Button3.TabIndex = 8
         Me.Button3.Text = "Camry"
         Me.Button3.UseVisualStyleBackColor = True
         '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(130, 219)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 9
+        Me.Button4.Text = "Corolla"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(130, 248)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 10
+        Me.Button5.Text = "Focus"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.Image = Global.BUTTIN.My.Resources.Resources._2021_Nissan_Maxima_2021_Nissan_Maxima_SR_Overview_Left
+        Me.PictureBox6.Location = New System.Drawing.Point(578, 96)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(156, 98)
+        Me.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox6.TabIndex = 16
+        Me.PictureBox6.TabStop = False
+        Me.PictureBox6.Visible = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = Global.BUTTIN.My.Resources.Resources._2022_toyota_corolla_angularfront2
+        Me.PictureBox5.Location = New System.Drawing.Point(578, 96)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(156, 98)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5.TabIndex = 15
+        Me.PictureBox5.TabStop = False
+        Me.PictureBox5.Visible = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.BUTTIN.My.Resources.Resources.fucis
+        Me.PictureBox4.Location = New System.Drawing.Point(578, 96)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(156, 98)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 14
+        Me.PictureBox4.TabStop = False
+        Me.PictureBox4.Visible = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.BUTTIN.My.Resources.Resources._2022_toyota_corolla_angularfront1
+        Me.PictureBox3.Location = New System.Drawing.Point(578, 96)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(156, 98)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 13
+        Me.PictureBox3.TabStop = False
+        Me.PictureBox3.Visible = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.BUTTIN.My.Resources.Resources._6367d51d_b6f8_4ea6_b444_6af6ee7ba03c
+        Me.PictureBox2.Location = New System.Drawing.Point(578, 96)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(156, 98)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 12
+        Me.PictureBox2.TabStop = False
+        Me.PictureBox2.Visible = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(262, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(156, 98)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 11
+        Me.PictureBox1.TabStop = False
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Label5)
@@ -114,6 +219,12 @@ Partial Class Form1
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -127,4 +238,12 @@ Partial Class Form1
     Friend WithEvents Label5 As Label
     Friend WithEvents Button2 As Button
     Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
 End Class
--- BUTTIN/BUTTIN/Form1.vb
+++ BUTTIN/BUTTIN/Form1.vb
@@ -1,12 +1,15 @@
 ﻿Public Class Form1
+    Dim cars(10) As car
     Structure car
         Dim make As String
         Dim model As String
         Dim hp As Integer
         Dim mpg As Integer
         Dim price As Decimal
+        Dim pic As Image
     End Structure
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        PictureBox1.Image = PictureBox2.Image
         Dim tt As car
         tt.make = "Audi"
         tt.model = "TT"
@@ -19,8 +22,8 @@
         Label4.Text = "Miles per Gallon:" + Str(tt.mpg)
         Label5.Text = "Price: " + tt.price.ToString("C2")
     End Sub
-
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        PictureBox1.Image = PictureBox6.Image
         Dim maxima As car
         maxima.make = "Nissan"
         maxima.model = "maxima"
@@ -33,8 +36,8 @@
         Label4.Text = "Miles per Gallon:" + Str(maxima.mpg)
         Label5.Text = "Price: " + maxima.price.ToString("C2")
     End Sub
-
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        PictureBox1.Image = PictureBox3.Image
         Dim Camry As car
         Camry.make = "Toyota"
         Camry.model = "Camry"
@@ -46,5 +49,33 @@
         Label3.Text = "Horse Power:" + Str(Camry.hp)
         Label4.Text = "Miles per Gallon:" + Str(Camry.mpg)
         Label5.Text = "Price: " + Camry.price.ToString("C2")
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        PictureBox1.Image = PictureBox5.Image
+        Dim Corolla As car
+        Corolla.make = "Toyota"
+        Corolla.model = "Corolla"
+        Corolla.hp = 169
+        Corolla.mpg = 31
+        Corolla.price = 20175
+        Label1.Text = "Make: " + Corolla.make
+        Label2.Text = "Model: " + Corolla.model
+        Label3.Text = "Horse Power:" + Str(Corolla.hp)
+        Label4.Text = "Miles per Gallon:" + Str(Corolla.mpg)
+        Label5.Text = "Price: " + Corolla.price.ToString("C2")
+    End Sub
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        PictureBox1.Image = PictureBox4.Image
+        Dim Focus As car
+        Focus.make = "Ford"
+        Focus.model = "Focus"
+        Focus.hp = 350
+        Focus.mpg = 30
+        Focus.price = 19000
+        Label1.Text = "Make: " + Focus.make
+        Label2.Text = "Model: " + Focus.model
+        Label3.Text = "Horse Power:" + Str(Focus.hp)
+        Label4.Text = "Miles per Gallon:" + Str(Focus.mpg)
+        Label5.Text = "Price: " + Focus.price.ToString("C2")
     End Sub
 End Class
